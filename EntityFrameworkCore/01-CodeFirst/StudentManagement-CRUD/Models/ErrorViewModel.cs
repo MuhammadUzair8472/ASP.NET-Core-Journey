89df48230ae752800b0afc0a1851e4af180b2ab0
Muhammad Uzair <138754762+MuhammadUzair8472@users.noreply.github.com>
2025-12-17 19:32:41 +0500

    Delete EntityFrameworkCore/01-CodeFirst/StudentManagement-CRUD directory
    bcz i create new

--- EntityFrameworkCore/01-CodeFirst/StudentManagement-CRUD/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace StudentManagment.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
